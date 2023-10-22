1ed3c56a5b2e8d7df7d7aa39712f72faa6f03b03
Chris Nguyen <chris.nguyen@hcss.com>
2023-10-21 23:47:53 -0500

    replace useContext with useUtils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const CreatePostWizard = () => {
 
   const [input, setInput] = useState("");
 
-  const ctx = api.useContext();
+  const ctx = api.useUtils();
 
   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
